d09f5e23a53eea5d2c9bf44335f5999ff9eb0e5f
u_m23qk <VButesku@alfabank.ru>
2026-06-07 16:46:29 +0300

    add logs

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
@@ -10,6 +12,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/films")
 public class FilmController {
+
+    private static final Logger log = LoggerFactory.getLogger(FilmController.class);
     private final FilmService filmService;
 
     public FilmController(FilmService filmService) {
@@ -18,16 +22,25 @@ public class FilmController {
 
     @GetMapping
     public List<Film> getAllFilms() {
-        return  filmService.getAllFilms();
+        log.info("GET /api/v1/films - получение всех фильмов");
+        List<Film> films = filmService.getAllFilms();
+        log.debug("GET /api/v1/films - найдено фильмов: {}", films.size());
+        return films;
     }
 
     @PostMapping
-    public Film addFilm(@RequestBody Film film) {
-        return filmService.createFilm(film);
+    public Film addFilm(@Valid @RequestBody Film film) {
+        log.info("POST /api/v1/films - добавление фильма: {}", film.getName());
+        Film createdFilm = filmService.createFilm(film);
+        log.info("POST /api/v1/films - фильм добавлен с id: {}", createdFilm.getId());
+        return createdFilm;
     }
 
     @PutMapping("/{id}")
-    public Film updateFilm(@Valid @RequestBody Film film) {
-        return filmService.updateFilm(film);
+    public Film updateFilm(@PathVariable Long id, @Valid @RequestBody Film film) {
+        log.info("PUT /api/v1/films/{} - обновление фильма: {}", id, film.getName());
+        Film updatedFilm = filmService.updateFilm(film);
+        log.info("PUT /api/v1/films/{} - фильм обновлен", id);
+        return updatedFilm;
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
@@ -10,6 +12,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
     public UserController(UserService userService) {
@@ -18,16 +22,25 @@ public class UserController {
 
     @GetMapping
     public List<User> getAllUsers() {
-        return userService.getAllUsers();
+        log.info("GET /api/v1/users - получение всех пользователей");
+        List<User> users = userService.getAllUsers();
+        log.debug("GET /api/v1/users - найдено пользователей: {}", users.size());
+        return users;
     }
 
     @PostMapping
     public User addUser(@Valid @RequestBody User user) {
-        return userService.createUser(user);
+        log.info("POST /api/v1/users - создание пользователя с логином: {}", user.getLogin());
+        User createdUser = userService.createUser(user);
+        log.info("POST /api/v1/users - пользователь создан с id: {}", createdUser.getId());
+        return createdUser;
     }
 
     @PutMapping("/{id}")
-    public User updateUser(@Valid @RequestBody User user) {
-        return userService.updateUser(user);
+    public User updateUser(@PathVariable Long id, @Valid @RequestBody User user) {
+        log.info("PUT /api/v1/users/{} - обновление пользователя", id);
+        User updatedUser = userService.updateUser(user);
+        log.info("PUT /api/v1/users/{} - пользователь обновлен", id);
+        return updatedUser;
     }
 }
